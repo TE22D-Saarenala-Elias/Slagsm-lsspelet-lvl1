c84a0fd32cf9b88eb00909f59ed42c1b8f7e4244
Elias Saarenala <elias.saarenala@elev.ga.ntig.se>
2023-09-21 10:57:12 +0200

    dfynjetjy

---
+++ Slagsmålsspelet lvl1/Program.cs
@@ -0,0 +1,24 @@
+﻿//SLUMP
+// Random generator = new Random();
+
+// int slumptal = generator.Next(10,20);
+
+// Console.WriteLine(slumptal);
+
+
+//WILLE-LOOP
+
+int tal = 100;
+
+while ( tal > 0)
+{
+    Console.WriteLine(tal);
+    tal--;
+}
+
+
+
+
+
+
+Console.ReadLine();
